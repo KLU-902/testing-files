0d912c37b31865ae6cef611a65b74bc420b0f02c
KLU-902 <82870305+KLU-902@users.noreply.github.com>
2021-04-26 10:15:29 +0530

    Update T1.sh

--- T1.sh
+++ T1.sh
@@ -1,21 +1,21 @@
-echo -e '
-──▐─▌──▐─▌──
-─▐▌─▐▌▐▌─▐▌─
-─█▄▀▄██▄▀▄█─
-──▄──██▌─▄──
-▄▀─█▀██▀█─▀▄
-▐▌▐▌─▐▌─▐▌▐▌
-─▐─█────█─▌─
-────▌──▐────
+
+
+
+
+
+
+
+
+
 '
-echo -e "                  CALL ME KLU" | pv -qL 10
-echo -e "             IAM FROM  K_L_U MODDING" | pv -qL 10
-echo -e "       IM A GEEK WITH LOTS OF EXCITEMENT" | pv -qL 10
-echo -e "             HOPE YOU LIKE THIS SCRIPT" | pv -qL 10
-echo -e "         OOPS... I TALK A LOT SRY FOR THAT " | pv -qL 10
-echo -e "              JOIN GROUPS ON WHATS' APP" | pv -qL 10
-echo -e "        MY WEBSITE: RELEASE SOON" | pv -qL 10
-echo -e "                BYEE.............." | pv -qL 10
+
+
+
+
+
+
+
+
 abot(){
 clear
 echo -e '
